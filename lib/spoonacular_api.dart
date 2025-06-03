1462ebd7ea4bc1f4a659c02ac0d0c2cf84c3edbf
James Allen <74887700+JimtheBurger@users.noreply.github.com>
2025-06-03 00:57:16 -0400

    added some stuff for pubspect and for the API

---
+++ lib/spoonacular_api.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class SpoonacularApi {
+  static final String _apiKey = dotenv.env['SPOONACULAR_API_KEY'] ?? '';
+  static const String _baseUrl = 'https://api.spoonacular.com';
+
+  static Future<List<dynamic>> searchRecipes(String query) async {
+    final uri = Uri.parse(
+      '$_baseUrl/recipes/complexSearch?query=${Uri.encodeQueryComponent(query)}&apiKey=$_apiKey',
+    );
+
+    final response = await http.get(uri);
+
+    if (response.statusCode != 200) {
+      throw Exception(
+          'Failed to search recipes (status code: ${response.statusCode})');
+    }
+
+    final Map<String, dynamic> jsonBody = json.decode(response.body);
+    if (!jsonBody.containsKey('results')) {
+      throw Exception('Unexpected response format: "results" key not found.');
+    }
+
+    return jsonBody['results'] as List<dynamic>;
+  }
+}
